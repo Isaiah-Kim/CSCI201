df8bdc22b153a695b43873357f79d43ceb11fbd7
Paul Lepe <paullepe@usc.edu>
2019-12-02 22:38:20 -0800

    Update VendingMachine.java

--- FrontEnd/src/VendingMachine.java
+++ FrontEnd/src/VendingMachine.java
@@ -1,16 +1,18 @@
 import java.util.ArrayList;
+import java.util.ArrayList;
 import java.util.List;
 
 public class VendingMachine {
 	List<Review> reviews = new ArrayList<Review>();
-	String location, latlng, itemValue, paymentValue, name, style;
-	double rating;
+	String location, itemValue, paymentValue, name, style;
+	double rating, latitude, longitude;
 	int id, raters;
 	
-	public VendingMachine(String n, String loc, String coord, String item, String payment,double aver, int rt, int machineId, List<Review> rev) {
+	public VendingMachine(String n, String loc, double lat, double lon, String item, String payment,double aver, int rt, int machineId, List<Review> rev) {
 		name = n;
 		location = loc; 
-		latlng = coord;
+		latitude = lat;
+		longitude = lon;
 		itemValue = item;
 		paymentValue = payment;
 		rating = aver;
@@ -33,14 +35,6 @@ public class VendingMachine {
 
 	public void setLocation(String location) {
 		this.location = location;
-	}
-
-	public String getLatlng() {
-		return latlng;
-	}
-
-	public void setLatlng(String latlng) {
-		this.latlng = latlng;
 	}
 
 	public String getItemValue() {
@@ -82,6 +76,44 @@ public class VendingMachine {
 	public void setId(int id) {
 		this.id = id;
 	}
+
+	public String getStyle() {
+		return style;
+	}
+
+	public void setStyle(String style) {
+		this.style = style;
+	}
+
+	public double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+
+	public double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+
+	public int getRaters() {
+		return raters;
+	}
+
+	public void setRaters(int raters) {
+		this.raters = raters;
+	}
+
+	public void setRating(double rating) {
+		this.rating = rating;
+	}
+	
+	
 	
 }
 
